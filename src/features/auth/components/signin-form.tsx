b7be170ed40a9aade23bacafd04ed363615a0d2b
jvictordev1 <jvictordev1@gmail.com>
2025-10-21 16:47:09 -0300

    feat: refatora formulario de login

--- src/features/auth/components/signin-form.tsx
+++ src/features/auth/components/signin-form.tsx
@@ -5,56 +5,43 @@ import { useForm } from 'react-hook-form';
 import { Input } from '@/components/ui/input';
 
 import { Form, FormField } from '@/components/ui/form';
-import { Separator } from '@/components/ui/separator';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { z } from 'zod';
 import { cn } from '@/lib/utils';
-import Link from 'next/link';
-import { Mail } from 'lucide-react';
-import { redirect } from 'next/dist/server/api-utils';
-import { useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 import { signinForm } from '../schemas/signinForm.schema';
 import { SigninFormType } from '../schemas/signinForm.schema';
+import { signIn } from '../services/authService';
+import { toast } from 'sonner';
+import { useRouter } from 'next/navigation';
+import useApiCall from '@/features/core/hooks/useApiCall';
+import { LoaderCircle } from 'lucide-react';
 
 export function SignInForm({
 	className,
-	redirectUrl = '/',
 	...props
 }: ComponentProps<'div'> & { redirectUrl?: string }) {
+	const { action, isLoading } = useApiCall(signIn);
 	const form = useForm<SigninFormType>({
 		resolver: zodResolver(signinForm),
 		defaultValues: {
-			email: '',
-			password: '',
+			matricula: '',
+			senha: '',
 		},
 	});
-
 	const router = useRouter();
 
-	const signInWithProvider = async (providerId: any) => {
-		// if (env.NODE_ENV === "production") {
-		//   toast.error("This feature is not available in production");
-		//   return;
-		// }
+	async function handleSignin(values: SigninFormType) {
+		try {
+			await action(values);
 
-		router.push('/dashboard');
-
-		// await authClient.signIn.social(
-		//   {
-		//     provider: providerId,
-		//     callbackURL: redirectUrl,
-		//   },
-		//   {
-		//     onRequest: () => {
-		//       toast.loading(`Redirecting to ${providerId}...`);
-		//     },
-		//     onResponse: () => {
-		//       toast.success("Signed in successfully");
-		//     },
-		//   }
-		// );
-	};
+			router.push('/dashboard');
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		} catch (error) {
+			toast.error(
+				'Houve um erro ao realizar o login, verifique as credenciais e tente novamente. Caso o problema persista entre em contato com o suporte.'
+			);
+		}
+	}
 
 	return (
 		<div
@@ -67,79 +54,51 @@ export function SignInForm({
 			<Form {...form}>
 				<form
 					className='flex min-w-sm flex-col gap-4'
-					onSubmit={(e) => {
-						e.preventDefault();
-						return null;
-					}}
+					onSubmit={form.handleSubmit(handleSignin)}
 				>
 					<FormField
 						control={form.control}
-						name='email'
+						name='matricula'
 						render={({ field }) => (
 							<Input
-								disabled
-								type='email'
-								placeholder='Email'
-								className='rounded-none !bg-[#1D1D1D]/100 text-base placeholder:text-[#9f9f9f]'
+								placeholder='Matricula'
+								className='rounded-none text-base placeholder:text-[#9f9f9f]'
+								disabled={isLoading}
 								{...field}
 							/>
 						)}
 					/>
+
 					<FormField
 						control={form.control}
-						name='password'
+						name='senha'
 						render={({ field }) => (
 							<Input
-								disabled
 								type='password'
-								placeholder='Password'
-								className='rounded-none !bg-[#1D1D1D]/100 text-base placeholder:text-[#9f9f9f]'
+								placeholder='Senha'
+								className='rounded-none text-base placeholder:text-[#9f9f9f]'
+								disabled={isLoading}
 								{...field}
 							/>
 						)}
 					/>
+
 					<Button
 						type='submit'
 						className='rounded-none text-base cursor-pointer'
-						disabled
+						disabled={isLoading}
 					>
-						Login
+						{isLoading ? (
+							<>
+								<LoaderCircle className='animate-spin' />
+								Carregando
+							</>
+						) : (
+							<>Login</>
+						)}
 					</Button>
-
-					<p className='text-sm text-[#9f9f9f]'>
-						Não tem uma conta?{' '}
-						<Link
-							href='/register'
-							className='py-2 font-medium text-white cursor-pointer hover:underline'
-						>
-							Registre-se
-						</Link>
-					</p>
 				</form>
 			</Form>
-
-			<div className='flex w-full max-w-sm flex-row items-center gap-2'>
-				<Separator
-					className='h-[2px] !w-[calc(50%-14px)] bg-[#2e2e2e]/100'
-					orientation='horizontal'
-				/>
-				<span className='text-sm text-[#9f9f9f]'>ou</span>
-				<Separator
-					className='h-[2px] !w-[calc(50%-14px)] bg-[#2e2e2e]/100'
-					orientation='horizontal'
-				/>
-			</div>
-
-			<div className='flex w-full flex-col gap-2'>
-				<Button
-					type='button'
-					className='border-border rounded-none border py-2 text-base transition-all cursor-pointer'
-					variant='outline'
-					onClick={() => signInWithProvider('github')}
-				>
-					<Mail className='h-4 w-4' /> Login com Gmail
-				</Button>
-			</div>
 		</div>
 	);
 }
--- src/features/auth/schemas/signinForm.schema.ts
+++ src/features/auth/schemas/signinForm.schema.ts
@@ -1,8 +1,8 @@
 import z from 'zod';
 
 export const signinForm = z.object({
-	email: z.email(),
-	password: z.string().min(8),
+	matricula: z.string(),
+	senha: z.string(),
 });
 
 export type SigninFormType = z.infer<typeof signinForm>;

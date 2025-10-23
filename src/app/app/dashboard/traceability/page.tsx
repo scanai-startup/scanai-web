d556200b233d4195ad6ade43f662d9afcb80c38c
jvictordev1 <jvictordev1@gmail.com>
2025-10-23 15:05:58 -0300

    feat: cria página de gestão de tanques

--- src/app/app/dashboard/traceability/page.tsx
+++ src/app/app/dashboard/traceability/page.tsx
@@ -3,8 +3,8 @@ import data from '../data.json';
 
 export default function Page() {
 	return (
-		<div className='bg-muted/50 rounded-xl'>
+		<>
 			<DataTable data={data} />
-		</div>
+		</>
 	);
 }

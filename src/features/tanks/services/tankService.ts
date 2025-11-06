74dab428cdf0f9f4602d0c51c6f573eff3c18445
jvictordev1 <jvictordev1@gmail.com>
2025-11-06 15:51:31 -0300

    feat: adiciona detalhamento de tanque especifico e refatora useApiCall

--- src/features/tanks/services/tankService.ts
+++ src/features/tanks/services/tankService.ts
@@ -1,23 +1,36 @@
 'use server';
 
-import { fakeDepositsWithData } from '@/factories/depositDataFactory';
 import { TankWithDetails } from '../types/tankWithDetails';
+import { apiService } from '@/features/core/services/apiService';
 
 export const getTanksWithData = async (): Promise<TankWithDetails[]> => {
 	try {
-		//TODO: testar com retorno da API
-		// const data = await apiService(
-		// 	'/deposito/getAllDepositosWithInformations',
-		// 	{
-		// 		method: 'GET',
-		// 	}
-		// );
+		const data = await apiService(
+			'/deposito/getAllDepositosWithInformations',
+			{
+				method: 'GET',
+			}
+		);
 
-		return await new Promise<TankWithDetails[]>((resolve) => {
-			setTimeout(() => resolve(fakeDepositsWithData), 3000);
-		});
+		return data as TankWithDetails[];
 	} catch (error) {
 		console.error('Erro ao buscar os dados dos tanques: ', error);
+		throw error;
+	}
+};
+
+export const getTankData = async (id: string): Promise<TankWithDetails> => {
+	try {
+		const data = await apiService(
+			`/deposito/getDepositoWithIdWithInformations/${id}`,
+			{
+				method: 'GET',
+			}
+		);
+
+		return data as TankWithDetails;
+	} catch (error) {
+		console.error('Erro ao buscar os dados do tanque. ', error);
 		throw error;
 	}
 };
